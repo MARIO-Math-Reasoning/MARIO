12488ddc827bdaa069cce49ab3e0d59e90652b2e
Fan, Kai <k.fan@alibaba-inc.com>
2024-01-19 19:11:44 -0800

    commit inference code

---
+++ batch_react.py
@@ -0,0 +1,173 @@
+"""
+author: lovecambi
+email: interfk@gmail.com
+"""
+from __future__ import annotations
+
+import os
+import json
+import argparse
+from termcolor import colored
+from tqdm import tqdm
+from vllm import LLM, SamplingParams
+from pebble import ProcessPool
+
+from react import ReactSolver, STOP
+
+
+TIMEOUT_SECONDS = 40
+
+
+def batch(iterable, n=-1):
+    l = len(iterable)
+    if n < 0:
+        n = l
+    for ndx in range(0, l, n):
+        yield iterable[ndx:min(ndx + n, l)]
+
+
+def generator(solver, output):
+    solver.step_generate(output)
+    return solver
+
+
+def batch_main(args, llm, sampling_params, questions, max_depth):
+
+    try:
+        solvers = [ReactSolver(args, question) for question in questions]
+
+        for step in tqdm(range(max_depth), desc="Step Processing"):
+            prompts = []
+            stop_words = []
+            epoch_solvers = []
+            next_solvers = []
+            for solver in solvers:
+                if solver.step_generate_flag():
+                    prompt_text = solver.get_llm_request()
+                    prompts.append(prompt_text)
+                    stop_words.extend(STOP)
+                    epoch_solvers.append(solver)
+                else:
+                    next_solvers.append(solver)
+            next_solver_span = len(next_solvers)
+            if len(epoch_solvers) < 1:
+                break
+            sampling_params.stop = list(set(stop_words))
+            outputs = llm.generate(prompts, sampling_params)
+
+            with ProcessPool(max_workers=min(len(epoch_solvers), os.cpu_count())) as pool:
+                future = pool.map(generator, epoch_solvers, outputs, timeout=TIMEOUT_SECONDS)
+                iterator = future.result()
+
+            if len(epoch_solvers) > 100:  
+                progress_bar = tqdm(total=len(epoch_solvers), desc="Execute")  
+            else:  
+                progress_bar = None 
+
+            while True:
+                try:
+                    result = next(iterator)
+                    next_solvers.append(result)
+                except StopIteration:
+                    break
+                except TimeoutError as error:
+                    next_solvers.append(None)
+                    print(error)
+                except Exception as error:
+                    print(error)
+                    next_solvers.append(None)
+                if progress_bar is not None:
+                    progress_bar.update(1) 
+            
+            if progress_bar is not None:
+                progress_bar.close() 
+            
+            # update solvers
+            assert len(epoch_solvers) == len(next_solvers[next_solver_span:]), f"Data is not matched, {len(epoch_solvers)} vs {len(next_solvers[next_solver_span:])}."
+            for idx, (ori_solver, new_solver) in enumerate(zip(epoch_solvers, next_solvers[next_solver_span:])):
+                if new_solver is None:
+                    next_solvers[next_solver_span + idx] = ori_solver
+            solvers = next_solvers
+
+    except Exception as e:
+        print(colored(f"Exception: {e}", "red"))
+        return [""] * len(questions)
+
+    jsonlines = {}
+    for solver in solvers:            
+        try:
+            solution = "\n\n".join(solver.step_texts)
+            jsonlines[solver.question] = json.dumps(solution, ensure_ascii=False)
+        except:
+            jsonlines[solver.question] = json.dumps(solution, ensure_ascii=False)
+    return jsonlines
+
+
+def main(args):
+    # init llm
+    available_gpus = os.environ.get('CUDA_VISIBLE_DEVICES', "0").split(',')
+    llm = LLM(
+        model=args.checkpoint_dir, 
+        tensor_parallel_size=len(available_gpus), 
+        trust_remote_code=False, 
+        seed=args.seed,
+    )
+    sampling_params = SamplingParams(
+        top_k=args.top_k,
+        best_of=args.best_of,
+        use_beam_search=args.use_beam_search,
+        temperature=args.temperature,
+        top_p=args.top_p,
+        max_tokens=args.max_tokens,
+        n=args.n_generate_sample,
+    )
+
+    # load question file
+    data = []
+    with open(args.question_file, "r") as f:
+        for line in f:
+            data.append(json.loads(line.strip()))
+
+    # write results
+    with open(f'{args.question_file}.prediction.jsonl', 'w') as writer:
+        for cur_data in tqdm(batch(data, args.num_per_inference), desc="Main Processing"):
+            questions = [d[args.question_key] for d in cur_data]
+            try:
+                jsonlines = batch_main(args, llm, sampling_params, questions, args.max_depth)
+                for d in cur_data:
+                    question = d[args.question_key]
+                    d["react"] = jsonlines[question]
+                    writer.write(json.dumps(d, ensure_ascii=False) + '\n')
+                    writer.flush()
+            except Exception as e:
+                print(colored(f"Batch Process Exception: {e}", "red"))
+
+
+def parse_args():
+    args = argparse.ArgumentParser()
+    args.add_argument('-q', '--question_file', type=str, required=True, help="file path of question file, should be jsonl format.")
+    args.add_argument('--question_key', type=str, default="question", help="questioin key in json")
+    args.add_argument('--num_per_inference', type=int, default=-1, help="number of questions per inference")
+    
+    # react
+    args.add_argument('-c', '--checkpoint_dir', type=str, required=True, help="folder of model checkpoint.")
+    args.add_argument('--max_depth', type=int, default=8, help="maximum step of solution")
+    args.add_argument('--verbose', action="store_true", help="print intermediate result on screen")
+
+    # vll
+    args.add_argument('--max_tokens', type=int, default=2048, help="decoding tokens")
+    args.add_argument('--temperature', type=float, default=0, help="for sampling")
+    args.add_argument('--top_k', type=int, default=-1, help="for sampling")
+    args.add_argument('--top_p', type=float, default=1.0, help="for sampling")
+    args.add_argument('--use_beam_search', action="store_true", help="use beam search")
+    args.add_argument('--best_of', type=int, default=1, help="for beam search")
+    args.add_argument('--n_generate_sample', type=int, default=1, help="number of generated samples")
+    args.add_argument('--seed', type=int, default=1234, help="random seed.")
+
+    args = args.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args)
---
+++ react.py
@@ -0,0 +1,230 @@
+"""
+author: lovecambi
+email: interfk@gmail.com
+"""
+from __future__ import annotations
+
+import re
+import argparse
+from termcolor import colored
+
+from vllm.outputs import RequestOutput
+from timeout_decorator import timeout
+
+from python_tool import PythonInterpreter
+
+
+SFT_PROMPT = "Question: {question}\n\nSolution:\n\n{partial_solution}"
+PRIMER = "<p>\n"
+STOP = ["\n</code>", "</code>"]
+CODE_LTAG = "<code>"
+CODE_RTAG = "</code>"
+CODE_OUTPUT = "Output: "
+
+
+python_interpreter = PythonInterpreter(globals=globals(), locals=None)
+
+
+def tool_wrapper(tool):
+    def _tool(query):
+        return tool.run(query)
+    return _tool
+
+
+class STEP(object):
+    
+    def __init__(self,
+        text: str = "",
+        code_snippet: str = "",
+        final_answer: str = "",
+        depth: int = 0,
+    ):
+        self.text = text
+        self.code_snippet = code_snippet
+        self.final_answer = final_answer
+        self.depth = depth
+
+        self.next_step = None
+        self.is_terminal = False
+
+
+class ReactSolver(object):
+
+    def __init__(self, args, question: str):
+        self.args = args
+        self.question = question
+
+        self.start_step = STEP()
+
+        self.current_step = self.start_step
+        self.step_texts = []
+        self.step_code_snippets = []
+
+    def step_generate_flag(self) -> bool:
+        return not self.current_step.is_terminal and self.current_step.depth <= self.args.max_depth
+    
+    def get_llm_request(self) -> str:
+        # get partial solution
+        _partial_solution = "\n\n".join(self.step_texts)
+        if _partial_solution:
+            partial_solution = f"{_partial_solution}\n\n{PRIMER}"
+        else:
+            partial_solution = f"{PRIMER}"
+        
+        # create prompt
+        prompt = SFT_PROMPT.format(question=self.question, partial_solution=partial_solution)
+        return prompt
+    
+    def step_generate(self, output: RequestOutput) -> None:
+        """process output from vllm
+        e.g.,
+
+        outputs = llm.generate(prompts, sampling_params)
+        for output in outputs:
+            step_generate(output)
+        """
+        sampled_step_result = (PRIMER + output.outputs[0].text).strip()
+
+        # parsing code snippet
+        step_result, parser_result = self.code_parser(sampled_step_result)
+
+        if self.args.verbose:
+            print(colored(f"{step_result}", "green"))
+
+        # create new step
+        new_step = STEP()
+        new_step.depth = self.current_step.depth + 1
+
+        if parser_result is None:
+            new_step.is_terminal = True
+            new_step.text = step_result
+        elif parser_result["final_answer"]:
+            new_step.is_terminal = True
+            new_step.text = step_result
+            new_step.final_answer = parser_result["final_answer"]
+        elif parser_result["code_snippet"]:
+            code_snippet = parser_result["code_snippet"]
+            observation = self.code_execution(code_snippet)
+            # update history
+            self.step_code_snippets.append(code_snippet)
+
+            if self.args.verbose:
+                print(colored(f"{CODE_OUTPUT}{observation}\n", "yellow"))
+            
+            new_step.text = f"{step_result}\n{CODE_OUTPUT}{observation}"
+            new_step.code_snippet = code_snippet
+        else:
+            new_step.text = step_result
+        
+        # update history
+        self.step_texts.append(new_step.text)
+        # update current step
+        self.current_step.next_step = new_step
+        self.current_step = new_step
+
+    def code_parser(self, text: str):
+        includes_answer = "Final Answer:" in text
+        regex = r"{code_ltag}[\s]*(.*)".format(code_ltag=CODE_LTAG)
+        code_match = re.search(regex, text, re.DOTALL)
+
+        parser_result = { 
+            "code_snippet": "",
+            "final_answer": "",
+        }
+
+        if code_match:
+            if includes_answer:
+                print(f"Warning: Incorrect format generated: `{text}`")
+                return text, None
+            
+            text = f"{text}\n{CODE_RTAG}"
+            code_snippet = code_match.group(1)
+            parser_result["code_snippet"] = code_snippet.strip(" ").strip('"')
+            return text, parser_result
+
+        elif includes_answer:
+            parser_result["final_answer"] = text.split("Final Answer:")[-1].strip()
+            return text, parser_result
+        
+        else:
+            print(f"Warning: Could not parse LLM output: `{text}`")
+            return text, None
+    
+    def code_execution(self, code_snippet: str) -> str:
+
+        @timeout(30)
+        def _code_execution(code_snippet: str) -> str:
+            tool_func = tool_wrapper(python_interpreter)
+
+            # first, execute history code snippets
+            for history_cs in self.step_code_snippets:
+                _ = tool_func(history_cs)
+            
+            # then, execute new code snippets
+            observation = str(tool_func(code_snippet))
+            del tool_func
+            return observation 
+        
+        try:
+            observation = _code_execution(code_snippet)
+        except Exception as e:
+            observation = "{}: {}".format(type(e).__name__, str(e))
+    
+        return observation
+
+
+def parse_args():
+    args = argparse.ArgumentParser()
+
+    args.add_argument('-c', '--checkpoint_dir', type=str, required=True, help="folder of model checkpoint.")
+    args.add_argument('--max_depth', type=int, default=8, help="maximum step of solution")
+    args.add_argument('--verbose', action="store_true", help="print intermediate result on screen")
+    args.add_argument('--temperature', type=float, default=0, help="for sampling")
+
+    args.add_argument('--question', type=str, default=None, help="question")
+
+    args = args.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    # the following script shows an example to solve one single question.
+    from vllm import LLM, SamplingParams
+
+    args = parse_args()
+
+    # init llm
+    llm = LLM(model=args.checkpoint_dir, tensor_parallel_size=1, trust_remote_code=True)
+    sampling_params = SamplingParams(
+        temperature=args.temperature,
+        top_k=-1,
+        top_p=1.0,
+        use_beam_search=False,
+        best_of=1,
+        max_tokens=2048, 
+        n=1, 
+        stop=STOP,
+    )
+
+    # define question and solver
+    if args.question:
+        question = args.question
+    else:
+        # an example question
+        question = "Given complex number $(a+i)(1-ai)=2,\;a \in \mathbb{R}$, find $a$."
+    if args.verbose:
+        print(colored(f"Question: {question}\n", "red"))
+    
+    solver = ReactSolver(args, question)
+
+    # run solver
+    while solver.step_generate_flag():
+        prompt = solver.get_llm_request()
+        prompts = [prompt]
+        outputs = llm.generate(prompts, sampling_params)
+        solver.step_generate(outputs[0])
+    
+    # save solution
+    full_solution = "\n\n".join(solver.step_texts)
+    with open("log.txt", "w") as f:
+        f.write(full_solution)
